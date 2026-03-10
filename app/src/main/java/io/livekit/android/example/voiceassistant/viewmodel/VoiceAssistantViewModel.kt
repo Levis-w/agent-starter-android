e13c808629bae691bdc0dbc6a4393e09d86be312
Levis-w <wlw82348589@gmail.com>
2026-03-11 02:51:51 +0800

    Update VoiceAssistantViewModel.kt

--- app/src/main/java/io/livekit/android/example/voiceassistant/viewmodel/VoiceAssistantViewModel.kt
+++ app/src/main/java/io/livekit/android/example/voiceassistant/viewmodel/VoiceAssistantViewModel.kt
@@ -1,36 +1,30 @@
 package io.livekit.android.example.voiceassistant.viewmodel
 
 import android.app.Application
+import android.content.Context
+import android.media.AudioManager
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.SavedStateHandle
 import androidx.navigation.toRoute
 import io.livekit.android.LiveKit
 import io.livekit.android.example.voiceassistant.screen.VoiceAssistantRoute
+import io.livekit.android.room.Room
 import io.livekit.android.token.TokenSource
 import io.livekit.android.token.cached
-import android.media.AudioManager
-import android.content.Context
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
-import io.livekit.android.room.RoomOptions
-import io.livekit.android.room.AudioOptions
-import io.livekit.android.room.AudioType
-/**
- * This ViewModel handles holding onto the Room object, so that it is
- * maintained across configuration changes, such as rotation.
- */
+
 class VoiceAssistantViewModel(application: Application, savedStateHandle: SavedStateHandle) : AndroidViewModel(application) {
+    
     private val audioManager = application.getSystemService(Context.AUDIO_SERVICE) as AudioManager
+    
     var isHiFiMode by mutableStateOf(true)
-    val room = LiveKit.create(
-        context = application,
-        options = RoomOptions(
-                      audioOptions = AudioOptions(audioOutputType = AudioType.Media)
-        )
-    )
-    fun toggleAudioMode() {
-        isHiFiMode = !isHiFiMode
+    
+    val room: Room = LiveKit.create(application)
+    
+    fun toggleAudioMode(isHiFi: Boolean) {
+        isHiFiMode = isHiFi
         if (isHiFiMode) {
             audioManager.mode = AudioManager.MODE_NORMAL
             audioManager.isSpeakerphoneOn = true
@@ -39,19 +33,17 @@ class VoiceAssistantViewModel(application: Application, savedStateHandle: SavedS
             audioManager.isSpeakerphoneOn = false
         }
     }
-
+    
     val tokenSource: TokenSource
-
     init {
         val (sandboxId, url, token) = savedStateHandle.toRoute<VoiceAssistantRoute>()
-
         tokenSource = if (sandboxId.isNotEmpty()) {
             TokenSource.fromSandboxTokenServer(sandboxId = sandboxId).cached()
         } else {
             TokenSource.fromLiteral(url, token).cached()
         }
     }
-
+    
     override fun onCleared() {
         super.onCleared()
         room.disconnect()
