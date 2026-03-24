babdd8a3e74fba14ade372ac469fb2468292cf10
Levis-w <wlw82348589@gmail.com>
2026-03-24 15:55:44 +0800

    Update VoiceAssistantViewModel.kt

--- app/src/main/java/io/livekit/android/example/voiceassistant/viewmodel/VoiceAssistantViewModel.kt
+++ app/src/main/java/io/livekit/android/example/voiceassistant/viewmodel/VoiceAssistantViewModel.kt
@@ -237,7 +237,7 @@ class VoiceAssistantViewModel(application: Application, savedStateHandle: SavedS
         if (!startInCallMode) {
             viewModelScope.launch {
                 Log.d("VoiceAssistant", "等待 1500ms 后开始自动切换...")
-                delay(3000)
+                delay(800)
                 Log.d("VoiceAssistant", "开始自动切换到 CALL_SPEAKER...")
                 switchAudioMode(AudioMode.CALL_SPEAKER)
             }
